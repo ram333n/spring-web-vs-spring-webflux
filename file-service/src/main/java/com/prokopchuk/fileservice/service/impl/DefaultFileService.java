855d938c7b3d9f027740ed9e5be65add1770a079
Roman Prokopchuk <ghost62727@gmail.com>
2025-03-02 00:15:43 +0200

    Handle saving files with the same name

--- file-service/src/main/java/com/prokopchuk/fileservice/service/impl/DefaultFileService.java
+++ file-service/src/main/java/com/prokopchuk/fileservice/service/impl/DefaultFileService.java
@@ -11,9 +11,11 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import lombok.RequiredArgsConstructor;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -52,16 +54,27 @@ public class DefaultFileService implements FileService {
 
     @Override
     public String importFile(InputStream stream, String fileName) {
-        File currentDir = getCurrentDirectory();
-        File fileToSave = new File(currentDir, fileName);
+        File fileToSave = createFileToSave(fileName);
 
         writeFile(fileToSave, stream);
 
         return saveFileData(fileToSave);
     }
 
-    private File getCurrentDirectory() {
-        return new File(mkdirService.mkdirCurrentDayDirectory(storageRootFolder));
+    private File createFileToSave(String fileName) {
+        try {
+            String currentDir = getCurrentDirectoryStr();
+            String prefix = FilenameUtils.getBaseName(fileName);
+            String suffix = FilenameUtils.getExtension(fileName);
+
+            return Files.createTempFile(Path.of(currentDir), prefix, "." + suffix).toFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getCurrentDirectoryStr() {
+        return mkdirService.mkdirCurrentDayDirectory(storageRootFolder);
     }
 
     private void writeFile(File file, InputStream content) {
